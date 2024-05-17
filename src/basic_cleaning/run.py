b4fdd175551059bb10eb9e6d418f0e99d4111123
Simogh67 <siavashmol@gmail.com>
2024-05-17 15:23:32 +0200

    basic_cleaning.py has been updated to drop rows that are not in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,10 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    # droping rows that are not in the proper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # saving to a csv
     df.to_csv("clean_sample.csv", index=False)
 
